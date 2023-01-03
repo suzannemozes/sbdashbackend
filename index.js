cc20bf8fe2b9096b49d11ba88f3405d9b5164ebd
Suzanne Mozes <suzanne.mozes@gmail.com>
2023-01-03 08:17:07 -0500

    add controllers, data, models, routes, index

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import contributionRoutes from "./routes/contribution.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+
+// CONFIG
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.use("/contribution", contributionRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+
+// MONGOOSE
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`server port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
