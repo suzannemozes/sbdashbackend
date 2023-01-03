00bfbda30612f2496541d59d65745d1742ecf459
Suzanne Mozes <suzanne.mozes@gmail.com>
2023-01-03 15:30:55 -0500

    sorting userId error

--- models/User.js
+++ models/User.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose"
 
-import mongoose from "mongoose";
-
 const UserSchema = new mongoose.Schema(
   {
-    name: {
+    donor_firstname: {
       type: String,
       required: true,
-      min: 2,
-      max: 100,
+
+    },
+    donor_lastname: {
+      type: String,
+      required: true,
+
     },
     email: {
       type: String,
@@ -21,9 +23,9 @@ const UserSchema = new mongoose.Schema(
       required: true,
       min: 5,
     },
-    city: String,
-    state: String,
-    country: String,
+    donor_city: String,
+    donor_state: String,
+    donor_zip: String,
     occupation: String,
     phoneNumber: String,
     transactions: Array,
@@ -35,6 +37,56 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+
+// id
+// 13763843
+// donor_firstname
+// "Cynthia"
+// donor_lastname
+// "Jeremy"
+// donor_city
+// "TUCSON"
+// donor_state
+// "AZ"
+// donor_zip
+// 85741
+// donor_is_eligible_for_express_lane
+// true
+// donor_phone
+// "+18881125520"
+// created_at
+// "2021-05-20 03:24:47+00"
+// order_number
+// BinData(0, 'U0IyMjIyMzc5MTg=')
+// contribution_form
+// "Cheetah"
+// refcodes
+// "{"refcode": "ref-Crane"}"
+// refcode
+// "ref-Crane"
+// recurring_period
+// "monthly"
+// recurring_duration
+// "infinite"
+// is_paypal
+// false
+// is_mobile
+// true
+// is_express
+// true
+// with_express_lane
+// false
+// express_signup
+// false
+// unique_identifier
+// "qxHMqZAvjF"
+// status
+// "approved"
+// text_message_option
+// "unknown"
+// custom_fields
+// "[]"
 
 const User = mongoose.model("User", UserSchema);
 export default User;
