37f35940d0b88328b2b7518576f35e81a79cb419
Suzanne Mozes <suzanne.mozes@gmail.com>
2023-01-05 06:20:41 -0500

    donor objects console.logging successfully

--- controllers/contribution.js
+++ controllers/contribution.js
@@ -1,5 +1,6 @@
 import Donor from "../models/Donor.js";
 import DonorStat from "../models/DonorStat.js";
+import User from "../models/User.js";
 
 export const getDonors = async (req, res) => {
   try {
@@ -8,7 +9,7 @@ export const getDonors = async (req, res) => {
     const donorsWithStats = await Promise.all(
       donors.map(async (donor) => {
         const stat = await DonorStat.find({
-          id: donor._id,
+          id: donor.id,
         });
         return {
           ...donor._doc,
@@ -17,7 +18,7 @@ export const getDonors = async (req, res) => {
       })
     );
 
-    res.status(200).json(donors);
+    res.status(200).json(donorsWithStats);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
